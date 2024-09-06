91a299ccbcd089b4667c4b7ee5bb92dd54c623fa
Ketan Patil <ketanpatil01@proton.me>
2024-09-06 21:41:20 +0530

    Prevent form submission if password fields are different
    The form would get submitted even if the 'Password' and
    'Confirm Password' fields had different values.
    This fixes that issue by preventing default action on form
    submission. The event listener needs to be added to the form instead of
    the submit button for this.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const form = document.querySelector("form");
 const password = document.querySelector("#password");
 const confirm_password = document.querySelector("#confirm_password");
-const submit_btn = document.querySelector("button");
 
-submit_btn.addEventListener("submit", () => {
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
     if (password.value !== confirm_password.value) {
         password.classList.add("password_error");
         confirm_password.classList.add("password_error");
+    } else {
+        form.submit()
     }
 })
