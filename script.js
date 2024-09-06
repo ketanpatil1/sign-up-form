841186bd8b82058c324769f1367bb5f9dea89161
Ketan Patil <ketanpatil01@proton.me>
2024-09-06 21:33:15 +0530

    Change event from click to submit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const password = document.querySelector("#password");
 const confirm_password = document.querySelector("#confirm_password");
 const submit_btn = document.querySelector("button");
 
-submit_btn.addEventListener("click", () => {
+submit_btn.addEventListener("submit", () => {
     if (password.value !== confirm_password.value) {
         password.classList.add("password_error");
         confirm_password.classList.add("password_error");
